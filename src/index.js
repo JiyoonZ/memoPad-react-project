0a9cc2bdfb88a79d9b2fe16102363978f36c3bbd
jiyoonZ <juneky416@gmail.com>
2022-08-11 01:26:50 +0900

    Style : 공통 스타일 및 테마 설정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import {ThemeProvider} from "styled-components";
+import theme from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
---
+++ src/theme.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const colors = {
+  mainGray: "#e4ebf5",
+  shadowGray: "#c8d0e7",
+  white: "#ffffff",
+  red: "#f24130",
+  blue: "#135EF2",
+  darkGray: "#262626",
+};
+
+const layout = {
+  flexbox: `
+    margin: 20px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  flexCenter: `
+  display: flex;
+  justify-contents: center;
+  align-items: center;
+`,
+  flexCenterColumn: `
+    display: flex;
+    flex-direction: column;
+    justify-contents: center;
+    align-items: center;
+  `,
+};
+
+const theme = {
+  colors,
+  layout,
+};
+
+export default theme;
