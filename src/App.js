a96fee7088eff98463193c23c4c341f7b8bf3680
jiyoonZ <juneky416@gmail.com>
2022-08-10 23:13:07 +0900

    Setting : router 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Main from "./pages/Main";
+import "./reset.css";
 function App() {
-  return <div>Zeeyoon MemoPad 📚</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
