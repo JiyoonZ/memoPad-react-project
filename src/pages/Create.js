51b9e79b92031600b8dedd2ecb6c4093ab300000
jiyoonZ <juneky416@gmail.com>
2022-08-14 02:12:46 +0900

    Feat: create 기능 로컬생성

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,47 +4,74 @@ import styled from "styled-components";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowLeftLong} from "@fortawesome/free-solid-svg-icons";
 import {useNavigate} from "react-router-dom";
+import dayjs from "dayjs";
+import {useState} from "react";
 
 function Create() {
   const navigate = useNavigate();
+  const existedEntry = JSON.parse(localStorage.getItem("memoList"));
+  const [memoId, setMemoId] = useState(
+    existedEntry ? existedEntry.length + 1 : 1
+  );
   function goBackHandler() {
+    navigate("/");
+  }
+  function submitHandler(evt) {
+    evt.preventDefault();
+    const memoEntry = {
+      id: memoId,
+      title: evt.target.title.value,
+      content: evt.target.content.value,
+      date: dayjs(new Date()).format("YYYY-MM-DD"),
+    };
+    if (existedEntry === null) {
+      const newEntry = [];
+      newEntry.push(memoEntry);
+      localStorage.setItem("memoList", JSON.stringify(newEntry));
+    } else {
+      existedEntry.push(memoEntry);
+      localStorage.setItem("memoList", JSON.stringify(existedEntry));
+    }
+    setMemoId((prev) => {
+      return prev + 1;
+    });
     navigate("/");
   }
   return (
     <div>
-      <Container>
-        <Flexbox>
-          <Button btnColor="back" onClick={goBackHandler}>
-            <FontAwesomeIcon icon={faArrowLeftLong} />
-            &nbsp; 뒤로가기
-          </Button>
-          <Button btnColor="blue">저장하기</Button>
-        </Flexbox>
-        <Form>
-          <TitleInput placeholder="제목을 입력해주세요." />
-          <ContentInput placeholder="내용을 입력해주세요." />
-        </Form>
-      </Container>
+      <form onSubmit={submitHandler}>
+        <Container>
+          <Flexbox>
+            <Button btnColor="back" onClick={goBackHandler}>
+              <FontAwesomeIcon icon={faArrowLeftLong} />
+              &nbsp; 뒤로가기
+            </Button>
+            <Button type="submit" btnColor="blue">
+              저장하기
+            </Button>
+          </Flexbox>
+
+          <TitleInput placeholder="제목을 입력해주세요." name="title" />
+          <ContentInput placeholder="내용을 입력해주세요." name="content" />
+        </Container>
+      </form>
     </div>
   );
 }
 const Flexbox = styled.div`
   ${({theme}) => theme.layout.flexbox}
 `;
-const Form = styled.form`
-  height: 80%;
-  margin-top: 40px;
-`;
 const TitleInput = styled.input`
   width: 100%;
   height: 40px;
   padding: 5px 15px;
+  margin-top: 10px;
   border-radius: 13px;
   box-sizing: border-box;
 `;
 const ContentInput = styled.textarea`
   width: 100%;
-  height: 80%;
+  height: 70%;
   border-radius: 13px;
   box-sizing: border-box;
   margin: 20px auto;
